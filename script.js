abf1f3e9d32735f10ac03fac93484978309906dd
Your Name <Neha>
2026-06-05 12:43:38 +0530

    Added S grade support

--- script.js
+++ script.js
@@ -7,27 +7,30 @@ function calculateCGPA() {
 
     let gradePoint = 0;
 
-    if (grade === "A+") {
-        gradePoint = 10;
-    }
-    else if (grade === "A") {
-        gradePoint = 9;
-    }
-    else if (grade === "B+") {
-        gradePoint = 8;
-    }
-    else if (grade === "B") {
-        gradePoint = 7;
-    }
-    else if (grade === "C") {
-        gradePoint = 6;
-    }
-    else if (grade === "P") {
-        gradePoint = 5;
-    }
-    else {
-        gradePoint = 0;
-    }
+if (grade === "S") {
+    gradePoint = 10;
+}
+else if (grade === "A+") {
+    gradePoint = 9.0;
+}
+else if (grade === "A") {
+    gradePoint = 8.5;
+}
+else if (grade === "B+") {
+    gradePoint = 8.0;
+}
+else if (grade === "B") {
+    gradePoint = 7.0;
+}
+else if (grade === "C") {
+    gradePoint = 6.0;
+}
+else if (grade === "P") {
+    gradePoint = 5.0;
+}
+else {
+    gradePoint = 0;
+}
 
     let cgpa = gradePoint;
 
